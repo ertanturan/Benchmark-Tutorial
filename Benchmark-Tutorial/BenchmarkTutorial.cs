edc829759d909d7970da444401d1b4d14a3cdbf2
Ertan Turan <ertanturan@outlook.com>
2023-03-01 22:55:03 +0000

    tutorial final state

--- Benchmark-Tutorial/BenchmarkTutorial.cs
+++ Benchmark-Tutorial/BenchmarkTutorial.cs
@@ -1,6 +1,55 @@
-﻿namespace Benchmark_Tutorial;
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Order;
 
+namespace Benchmark_Tutorial;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+[InvocationCount(100)]
+[BenchmarkCategory("StringOperations")]
+[BaselineColumn]
 public class BenchmarkTutorial
 {
     
+    
+    [Params( 100,500, 1000)] 
+    public int iterationCount;
+
+    [Benchmark]
+    public void StringConcat()
+    {
+        string temp = string.Empty;
+
+        for (int i = 0; i < iterationCount; i++)
+        {
+            temp = string.Concat(temp, "a");
+        }
+    }
+
+    [Benchmark(Baseline = true)]
+    public void StringConcatUsingPlus()
+    {
+        string temp = string.Empty;
+
+        for (int i = 0; i < iterationCount; i++)
+        {
+            temp += "a";
+        }
+    }
+
+    [Benchmark]
+    public void StringBuilderTest()
+    {
+        StringBuilder temp = new StringBuilder();
+
+        for (int i = 0; i < iterationCount; i++)
+        {
+            temp.Append("a");
+        }
+    }
+
+
 }
---
+++ Benchmark-Tutorial/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Benchmark_Tutorial;
+using BenchmarkDotNet.Running;
+
+Console.WriteLine("Hello, World!");
+
+BenchmarkRunner.Run<BenchmarkTutorial>();
